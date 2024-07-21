1d256a265363fb4f2487219e33e416de06ef89a4
iyed <iyed.mohamed@isimg.tn>
2024-07-21 19:54:16 +0100

    Finishing the Login Function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"; // to hash the password
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
     try{
@@ -14,7 +16,8 @@ export const signup = async (req, res) => {
         }
 
         //Hash password here
-
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = await bcrypt.hashSync(password, salt);
         //the avatar api link : https://avatar-placeholder.iran.liara.run/
 
         const boyProfilePic = `https://avatar-placeholder.iran.liara.run/public/boy?username=${userName}`
@@ -23,10 +26,14 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             userName,
-            password,
+            password:hashPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
         });
+        
+        if(newUser) {
+        //Generate JWT token here
+        generateTokenAndSetCookie(newUser._id, res);
         await newUser.save()
 
         res.status(201).json({
@@ -34,8 +41,10 @@ export const signup = async (req, res) => {
             fullName: newUser.fullName,
             userName: newUser.userName,
             profilePic: newUser.profilePic,
-
         })
+        } else{
+            res.status(400).json({message: "Invalid User Data "})
+        }
 
     } catch (error) {
         console.log("error in signup controller", error.message);
@@ -45,9 +54,28 @@ export const signup = async (req, res) => {
 
 };
 
-export const login = (req, res) => {
-    res.send("Login User")
-    console.log("login user");
+export const login = async (req, res) => {
+    try {
+        const {userName, password} = req.body;
+        const user = await User.findOne({userName});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({message: "Invalid Username or Password "});
+        }
+        //Generate JWT token here
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user.fullName,
+            userName: user.userName,
+            profilePic: user.profilePic
+        })
+
+    }catch (error) {
+        console.log("error in login controller", error.message);
+        res.status(500).json({error:" Internal Server Error "});
+    }
 };
 
 export const logout = (req, res) => {
