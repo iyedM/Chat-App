d52a6be2a350ee5eb2c2ed6f15d782560d971f61
iyed <iyed.mohamed@isimg.tn>
2024-07-21 18:35:54 +0100

    "Updated .env file and auth.controller.js to add JWT secret, node env, and password hashing"

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
     try{
-        const {fullName, userName, password, confirmPassword, gender} = req.body;
+        const {fullName, userName, password, confirmPassword, gender, profilePic} = req.body;
         if(password !== confirmPassword) {
              return res.status(400).json({message: "Password and Confirm are different"})
              }
@@ -14,7 +16,30 @@ export const signup = async (req, res) => {
         //Hash password here
 
         //the avatar api link : https://avatar-placeholder.iran.liara.run/
+
+        const boyProfilePic = `https://avatar-placeholder.iran.liara.run/public/boy?username=${userName}`
+        const girlProfilePic = `https://avatar-placeholder.iran.liara.run/public/girld?username=${userName}`
+
+        const newUser = new User({
+            fullName,
+            userName,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+        await newUser.save()
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            userName: newUser.userName,
+            profilePic: newUser.profilePic,
+
+        })
+
     } catch (error) {
+        console.log("error in signup controller", error.message);
+        res.status(500).json({error:" Internal Server Error "});
 
     }
 
